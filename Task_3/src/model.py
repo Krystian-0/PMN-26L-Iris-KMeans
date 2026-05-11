a0f8825dd0aff9b30c2b44eab5392adc5b537faf
Krystian <krystianosak573@z_githuba.com>
2026-05-12 00:00:16 +0200

    Dodano Seri 4 (Analiza Poolingu) i kocowe wyniki

--- Task_3/src/model.py
+++ Task_3/src/model.py
@@ -4,57 +4,54 @@ import torch.nn.functional as F
 
 
 class CustomCNN(nn.Module):
-    def __init__(self, kernel_size=3, pool_type='max', num_classes=10):
+    def __init__(self, kernel_size=3, pool_type='max'):
         super(CustomCNN, self).__init__()
-
+        self.pool_type = pool_type.lower()
         padding = kernel_size // 2
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=kernel_size, padding=padding)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size, padding=padding)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=kernel_size, padding=padding)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=kernel_size, padding=padding)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=kernel_size, padding=padding)
 
-        self.pool_type = pool_type
-        if pool_type == 'max':
-            self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        elif pool_type == 'avg':
-            self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+        if self.pool_type == 'avg':
+            self.pool = nn.AvgPool2d(2, 2)
         else:
-            self.pool = nn.Identity()
+            self.pool = nn.MaxPool2d(2, 2)
 
         self.dropout = nn.Dropout(0.3)
 
-        self._to_linear = None
-        self._dummy_forward(torch.zeros(1, 3, 32, 32))
-
-        self.fc1 = nn.Linear(self._to_linear, 256)
-        self.fc2 = nn.Linear(256, num_classes)
-
-    def _dummy_forward(self, x):
-        x = self.conv1(x)
-        x = self.pool(x)
-        x = self.conv2(x)
-        x = self.pool(x)
-        x = self.conv3(x)
-        x = self.pool(x)
-        self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x, return_features=False):
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self._apply_pooling(x)
 
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self._apply_pooling(x)
 
-        x = F.relu(self.conv3(x))
-        x = self.pool(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self._apply_pooling(x)
 
-        x = torch.flatten(x, 1)
+        x = x.view(-1, 128 * 4 * 4)
         features = x
 
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
+        x = F.relu(x)
         x = self.dropout(x)
-        out = self.fc2(x)
+        x = self.fc2(x)
 
         if return_features:
-            return out, features
-        return out
+            return x, features
+        return x
+
+    def _apply_pooling(self, x):
+        if self.pool_type == 'min':
+            return -self.pool(-x)
+        elif self.pool_type == 'none':
+            return x
+        else:
+            return self.pool(x)
